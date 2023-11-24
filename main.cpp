6ecb83cc4b52bd3730e9eea1be9211e8cdd3c500
m-ismail-ramzan <m.ismail_ramzan@yahoo.com>
2023-11-24 11:31:21 +0500

    test_this

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <iterator>
 #include <map>
 #include <set>
+#include <climits>
 
 using namespace std;
 
@@ -114,11 +115,102 @@ void initArray(int **arr, int row, int col)
     }
 }
 
-// the chain matrix multiplication function
-// will get the optimal solution for multiplying
-void chainMatrixMultiplication(int arr[], int size)
+// Function for matrix chain multiplication
+int MatrixChainOrder(int dimensions[], int n, int brackets[])
 {
-    
+    int m[n][n];
+
+    int i, j, k, L, q;
+
+    // cost is zero when multiplying one matrix.
+    for (i = 1; i < n; i++)
+        m[i][i] = 0;
+
+    // L is chain length.
+    for (L = 2; L < n; L++)
+    {
+        for (i = 1; i < n - L + 1; i++)
+        {
+            j = i + L - 1;
+            m[i][j] = INT_MAX;
+            for (k = i; k <= j - 1; k++)
+            {
+                // q = cost/scalar multiplications
+                q = m[i][k] + m[k + 1][j] + dimensions[i - 1] * dimensions[k] * dimensions[j];
+                if (q < m[i][j])
+                {
+                    m[i][j] = q;
+                    brackets[i + j * n] = k;  // Store the value of k
+                }
+            }
+        }
+    }
+
+    return m[1][n - 1];
+}
+
+// Function to print the optimal matrix chain multiplication
+void printOptimalParenthesis(int i, int j, int n, int brackets[])
+{
+    if (i == j)
+    {
+        printf("A%d", i);
+    }
+    else
+    {
+        printf("(");
+        printOptimalParenthesis(i, brackets[i + j * n], n, brackets);
+        printOptimalParenthesis(brackets[i + j * n] + 1, j, n, brackets);
+        printf(")");
+    }
+}
+
+// a function that will return the dimentions after reading the array
+//2 3 
+//3 4 
+//4 2 
+//2 1 
+// i want 2 3 4 2 1
+
+int* getDimentions(int **arr, int row, int col)
+{   
+    int index=0;
+    int *mydimention= new int[TOTALARRAYS+1];
+
+    for (int i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < col; ++j)
+        {
+            // get the first index of the first array
+            if(i == 0 && j == 0)
+            {
+                mydimention[index] = arr[i][j];
+                 index++;
+                cout << "\n first index:" << arr[i][j] << "\n ";
+               
+            }
+            // get the second index of the all other arrays
+            else if(i != 0 && j == 0)
+            {
+                  mydimention[index] = arr[i][j];
+                 index++;
+                cout <<"\n Last index: " <<arr[i][j] << "\n ";
+            }
+            // get the last index of the last array
+            else if(i == row - 1 && j == col - 1)
+            {   mydimention[index] = arr[i][j];
+                 index++;
+                cout << "\n Final index: " << arr[i][j] << "\n ";
+            }
+            else
+            {
+               // cout << arr[i][j] << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return mydimention;
 }
 
 // Function: main
@@ -154,12 +246,36 @@ int main()
     }
 
     // // print the array
-    // for (int i = 0; i < TOTALARRAYS; i++)
-    // {
-    //     cout << "Array " << i + 1 << endl;
-    //     printArray(array[i], dimensions[i][0], dimensions[i][1]);
-    //     cout << endl;
-    // }
+     for (int i = 0; i < TOTALARRAYS; i++)
+     {
+         cout << "Array " << i + 1 << endl;
+         printArray(array[i], dimensions[i][0], dimensions[i][1]);
+        cout << endl;
+     }
+
+    // getting the dimentions into the right format as needed
+    cout << "Total Dimentions  are: " << TOTALARRAYS + 1 << endl;
+    int* mydimentions = getDimentions(dimensions, TOTALARRAYS, 2);
+
+    // printing the mydimentions array
+    cout << "\nFetched the Dimentions\n";
+    for (int i = 0; i < TOTALARRAYS + 1; i++)
+    {
+        cout << mydimentions[i] << " ";
+    }    
+    cout << "\n------\n";
+    // chain multiplication for the above arrays to get the optimal solution
+    //int mydim[] = {2,3,4,2,1};
+    int n = TOTALARRAYS+1;
+    cout << "\nn is " << n << "\n";
+    int mybrak[n * n] = {0};
+
+    printf("Optimal Cost: %d\n", MatrixChainOrder(mydimentions, n, mybrak));
+    printf("Optimal Parenthesization: ");
+    printOptimalParenthesis(1, n - 1, n, mybrak);
+
+
+    delete[] mydimentions;
 
     cout << "Hello World!" << endl;
     return 0;
